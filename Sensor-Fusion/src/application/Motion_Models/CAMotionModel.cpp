f57fe677a855cd4eda9240ed404394af306f8859
Bharadwaj <bharath.krishnan02@gmail.com>
2021-01-10 13:02:06 +0100

    Added CV and CA Motion Models

---
+++ Sensor-Fusion/src/application/Motion_Models/CAMotionModel.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : CAMotionModel.cpp
+// Author      : Bharadwaj
+// Version     : -
+// Copyright   : Your copyright notice
+// Description : Class Definition for a Constant Acceleration Motion Model
+//============================================================================
+
+#include "CAMotionModel.h"
+
+CAMotionModel::CAMotionModel()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
